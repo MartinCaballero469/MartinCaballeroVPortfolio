a8f8df48ec07633e4754193f661d571990fab65b
Martin Caballero <martin.caballero678@gamil.com>
2022-01-25 13:58:04 -0600

    Beta v1.2 update redirectPage.html

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const WEATHER_API_KEY = "c58b170be5ec4b688b2194913222101";
 
 const api_url = "https://api.weatherapi.com/v1/current.json?key=";
 var weatherRequest = new Request('https://api.weatherapi.com/v1/current.json?key=c58b170be5ec4b688b2194913222101&q=London&aqi=no');
-
+function choosePath() {
+  document.getElementById("progSec1").style.width = "33.33%";
+}
 function startWalk() {
     document.getElementById("StartScreen").classList.add("d-none");
     document.getElementById("ProgSelection").classList.remove("d-none");
     document.getElementById("IntroBox").classList.remove("align-items-center");
-
 }
 
 fetch(weatherRequest)
