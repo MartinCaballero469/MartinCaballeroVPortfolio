f2385b065a339e330b5f3bee710664b3118b417b
Martin Caballero <martin.caballero678@gamil.com>
2022-02-14 20:03:01 -0600

    Beta v1.3

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const WEATHER_API_KEY = "c58b170be5ec4b688b2194913222101";
+(function ($){
+  $.fn.counter = function() {
+    const $this = $(this),
+    numberFrom = parseInt($this.attr('data-from')),
+    numberTo = parseInt($this.attr('data-to')),
+    delta = numberTo - numberFrom,
+    deltaPositive = delta > 0 ? 1 : 0,
+    time = parseInt($this.attr('data-time')),
+    changeTime = 10;
+    
+    let currentNumber = numberFrom,
+    value = delta*changeTime/time;
+    var interval1;
+    const changeNumber = () => {
+      currentNumber += value;
+      //checks if currentNumber reached numberTo
+      (deltaPositive && currentNumber >= numberTo) || (!deltaPositive &&currentNumber<= numberTo) ? currentNumber=numberTo : currentNumber;
+      this.text(parseInt(currentNumber));
+      currentNumber == numberTo ? clearInterval(interval1) : currentNumber;  
+    }
 
-const api_url = "https://api.weatherapi.com/v1/current.json?key=";
-var weatherRequest = new Request('https://api.weatherapi.com/v1/current.json?key=c58b170be5ec4b688b2194913222101&q=London&aqi=no');
-function choosePath() {
-  document.getElementById("progSec1").style.width = "33.33%";
-}
-function startWalk() {
-    document.getElementById("StartScreen").classList.add("d-none");
-    document.getElementById("ProgSelection").classList.remove("d-none");
-    document.getElementById("IntroBox").classList.remove("align-items-center");
-}
+    interval1 = setInterval(changeNumber,changeTime);
+  }
+}(jQuery));
+
+$(document).ready(function(){
+
+  $('.count-up').counter();
+  $('.count1').counter();
+  $('.count2').counter();
+  
+  new WOW().init();
+  
+  setTimeout(function () {
+    $('.count5').counter();
+  }, 3000);
+});
+
 
 fetch(weatherRequest)
   .then(response => {
@@ -24,5 +50,27 @@ fetch(weatherRequest)
     console.log(data.location.name) })
   .catch(console.error);;
 
-
-
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
